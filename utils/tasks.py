ba974b393b4a84def0aa6295ed74702aec523d7d
Sarveshwar Senthil Kumar <sarveshwar313@gmail.com>
2025-04-11 20:20:08 -0400

    Added view deleted tasks

--- utils/tasks.py
+++ utils/tasks.py
@@ -45,7 +45,7 @@ def getTasks():
         return redirect("/")
     else:
         db = SQL("sqlite:///databases/tasks.db")
-        tasks = db.execute("SELECT * FROM tasks WHERE ownedBy = :id", id=session.get("id"))
+        tasks = db.execute("SELECT * FROM tasks WHERE ownedBy = :id AND status != :status", id=session.get("id"), status="Deleted")
 
         for task in tasks:
             task["additionalFields"] = json.loads(task["additionalFields"])
@@ -78,13 +78,27 @@ def edit_task(task_id):
     db.execute("UPDATE tasks SET title = :title, description = :description, status = :status, priority = :priority, dueDate = :dueDate, assignedTo = :assignedTo,category = :category, notes = :notes, additionalFields = :additionalFields, additionalValues = :additionalValues WHERE id = :id AND ownedBy = :owner", title=request.form.get("title"), description=request.form.get("description"), status=request.form.get("status"), priority=request.form.get("priority"), dueDate=request.form.get("dueDate"), assignedTo=request.form.get("assignedTo"), category=request.form.get("category"), notes=request.form.get("notes"), additionalFields=json.dumps(additional_fields), additionalValues=json.dumps(additional_values), id=task_id, owner=session.get("id"))
     
     db = SQL("sqlite:///databases/tasks.db")
-    tasks = db.execute("SELECT * FROM tasks WHERE ownedBy = :id", id=session.get("id"))
+    tasks = db.execute("SELECT * FROM tasks WHERE ownedBy = :id AND status != :status", id=session.get("id"), status="Deleted")
 
     for task in tasks:
         task["additionalFields"] = json.loads(task["additionalFields"])
         task["additionalValues"] = json.loads(task["additionalValues"])
 
     return render_template("viewTasks.html", tasks=tasks, error="Task has been updated successfully!", success=True)
+
+@tasks_blueprint.route("/checkdeleted", methods=["GET"])
+def viewdeleted():
+    if not session.get("name"):
+        return redirect("/")
+    
+    db = SQL("sqlite:///databases/tasks.db")
+    tasks = db.execute("SELECT * FROM tasks WHERE ownedBy = :id AND status = :status", id=session.get("id"), status="Deleted")
+
+    for task in tasks:
+        task["additionalFields"] = json.loads(task["additionalFields"])
+        task["additionalValues"] = json.loads(task["additionalValues"])
+
+    return render_template("viewTasks.html", tasks=tasks, deleted=True)
 
 @tasks_blueprint.route("/deletetask/<int:task_id>", methods=["POST"])
 def delete_task(task_id):
@@ -109,7 +123,7 @@ def delete_task(task_id):
         db.execute("UPDATE tasks SET status = :status WHERE id = :id AND ownedBy = :owner", status="Deleted", id=task_id, owner=session.get("id"))
 
     db = SQL("sqlite:///databases/tasks.db")
-    tasks = db.execute("SELECT * FROM tasks WHERE ownedBy = :id", id=session.get("id"))
+    tasks = db.execute("SELECT * FROM tasks WHERE ownedBy = :id AND status != :status", id=session.get("id"), status="Deleted")
 
     for task in tasks:
         task["additionalFields"] = json.loads(task["additionalFields"])
@@ -117,7 +131,7 @@ def delete_task(task_id):
 
     return render_template("viewTasks.html", tasks=tasks, error="Task has been deleted successfully!", success=True)
 
-@tasks_blueprint.route("/recovertask/<int:task_id>", methods=["GET"])
+@tasks_blueprint.route("/recovertask/<int:task_id>", methods=["GET", "POST"])
 def recover_task(task_id):
     if not session.get("name"):
         return redirect("/")
@@ -146,7 +160,7 @@ def recover_task(task_id):
         db.execute("UPDATE tasks SET status = :status WHERE id = :id AND ownedBy = :owner", status=status, id=task_id, owner=session.get("id"))
 
     db = SQL("sqlite:///databases/tasks.db")
-    tasks = db.execute("SELECT * FROM tasks WHERE ownedBy = :id", id=session.get("id"))
+    tasks = db.execute("SELECT * FROM tasks WHERE ownedBy = :id AND status != :status", id=session.get("id"), status="Deleted")
 
     for task in tasks:
         task["additionalFields"] = json.loads(task["additionalFields"])
