aef6f582aac131f0429d763300a215532d808e2b
Sarveshwar Senthil Kumar <sarveshwar313@gmail.com>
2025-04-13 18:24:07 -0400

    Added backend compatibility for finish task

--- utils/tasks.py
+++ utils/tasks.py
@@ -45,13 +45,61 @@ def getTasks():
         return redirect("/")
     else:
         db = SQL("sqlite:///databases/tasks.db")
-        tasks = db.execute("SELECT * FROM tasks WHERE ownedBy = :id AND status != :status", id=session.get("id"), status="Deleted")
+        tasks = db.execute("SELECT * FROM tasks WHERE ownedBy = :id AND status != :status AND status != :status2", id=session.get("id"), status="Deleted", status2="Finished")
 
         for task in tasks:
             task["additionalFields"] = json.loads(task["additionalFields"])
             task["additionalValues"] = json.loads(task["additionalValues"])
 
         return render_template("viewTasks.html", tasks=tasks)
+
+@tasks_blueprint.route("/finishtask/<int:task_id>", methods=["POST"])
+def finish_task(task_id):
+    if not session.get("name"):
+        return redirect("/")
+    
+    db = SQL("sqlite:///databases/tasks.db")
+    
+    task = db.execute("SELECT * FROM tasks WHERE id = :id AND ownedBy = :owner", 
+                     id=task_id, owner=session.get("id"))
+    if not task:
+        db = SQL("sqlite:///databases/tasks.db")
+        tasks = db.execute("SELECT * FROM tasks WHERE ownedBy = :id", id=session.get("id"))
+
+        for task in tasks:
+            task["additionalFields"] = json.loads(task["additionalFields"])
+            task["additionalValues"] = json.loads(task["additionalValues"])
+
+        return render_template("viewTasks.html", tasks=tasks, error="Task cannot be found!")
+    
+    else:
+        tz_NY = pytz.timezone('America/New_York') 
+        now = datetime.now(tz_NY)
+        completedAt = now.strftime("%d/%m/%Y %H:%M:%S")
+
+        db.execute("UPDATE tasks SET status = :status, completedAt = :completedAt WHERE id = :id AND ownedBy = :owner", status="Finished", completedAt=completedAt, id=task_id, owner=session.get("id"))
+
+    db = SQL("sqlite:///databases/tasks.db")
+    tasks = db.execute("SELECT * FROM tasks WHERE ownedBy = :id AND status != :status AND status != :status2", id=session.get("id"), status="Deleted", status2="Finished")
+
+    time1 = datetime.strptime(task[0]["createdAt"], "%d/%m/%Y %H:%M:%S")
+    time2 = datetime.strptime(completedAt, "%d/%m/%Y %H:%M:%S")
+
+    # Calculate difference
+    difference = time2 - time1  # returns a timedelta
+
+    # Break down difference
+    days = difference.days
+    hours, remainder = divmod(difference.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+
+    duration = (f"{days} days, {hours} hours, {minutes} minutes, and {seconds} seconds!")
+
+    for task in tasks:
+        task["additionalFields"] = json.loads(task["additionalFields"])
+        task["additionalValues"] = json.loads(task["additionalValues"])
+
+    return render_template("viewTasks.html", tasks=tasks, error="Congratulations on finishing the task, it took you " + duration, success=True)
 
 @tasks_blueprint.route("/edittask/<int:task_id>", methods=["POST"])
 def edit_task(task_id):
@@ -78,7 +126,7 @@ def edit_task(task_id):
     db.execute("UPDATE tasks SET title = :title, description = :description, status = :status, priority = :priority, dueDate = :dueDate, assignedTo = :assignedTo,category = :category, notes = :notes, additionalFields = :additionalFields, additionalValues = :additionalValues WHERE id = :id AND ownedBy = :owner", title=request.form.get("title"), description=request.form.get("description"), status=request.form.get("status"), priority=request.form.get("priority"), dueDate=request.form.get("dueDate"), assignedTo=request.form.get("assignedTo"), category=request.form.get("category"), notes=request.form.get("notes"), additionalFields=json.dumps(additional_fields), additionalValues=json.dumps(additional_values), id=task_id, owner=session.get("id"))
     
     db = SQL("sqlite:///databases/tasks.db")
-    tasks = db.execute("SELECT * FROM tasks WHERE ownedBy = :id AND status != :status", id=session.get("id"), status="Deleted")
+    tasks = db.execute("SELECT * FROM tasks WHERE ownedBy = :id AND status != :status AND status != :status2", id=session.get("id"), status="Deleted", status2="Finished")
 
     for task in tasks:
         task["additionalFields"] = json.loads(task["additionalFields"])
@@ -123,7 +171,7 @@ def delete_task(task_id):
         db.execute("UPDATE tasks SET status = :status WHERE id = :id AND ownedBy = :owner", status="Deleted", id=task_id, owner=session.get("id"))
 
     db = SQL("sqlite:///databases/tasks.db")
-    tasks = db.execute("SELECT * FROM tasks WHERE ownedBy = :id AND status != :status", id=session.get("id"), status="Deleted")
+    tasks = db.execute("SELECT * FROM tasks WHERE ownedBy = :id AND status != :status AND status != :status2", id=session.get("id"), status="Deleted", status2="Finished")
 
     for task in tasks:
         task["additionalFields"] = json.loads(task["additionalFields"])
@@ -160,7 +208,7 @@ def recover_task(task_id):
         db.execute("UPDATE tasks SET status = :status WHERE id = :id AND ownedBy = :owner", status=status, id=task_id, owner=session.get("id"))
 
     db = SQL("sqlite:///databases/tasks.db")
-    tasks = db.execute("SELECT * FROM tasks WHERE ownedBy = :id AND status != :status", id=session.get("id"), status="Deleted")
+    tasks = db.execute("SELECT * FROM tasks WHERE ownedBy = :id AND status != :status AND status != :status2", id=session.get("id"), status="Deleted", status2="Finished")
 
     for task in tasks:
         task["additionalFields"] = json.loads(task["additionalFields"])
